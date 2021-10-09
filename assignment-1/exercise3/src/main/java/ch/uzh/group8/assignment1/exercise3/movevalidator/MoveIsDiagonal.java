db597d350fabee364efd1693869d243676083482
BacLuc <lucius.bachmann@gmx.ch>
2021-10-09 13:39:24 +0200

    assignment1: implement MoveIsDiagonal

--- assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/MoveIsDiagonal.java
+++ assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/MoveIsDiagonal.java
@@ -6,6 +6,8 @@ import ch.uzh.group8.assignment1.exercise3.Move;
 public class MoveIsDiagonal implements MoveValidator {
   @Override
   public boolean validate(Move move, Board board) {
-    return true;
+    var start = move.start();
+    var end = move.end();
+    return start.row() != end.row() && start.column() != end.column();
   }
 }
