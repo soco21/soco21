5962d6c8103ea6f4788ce99b9dd70419297e611b
BacLuc <lucius.bachmann@gmx.ch>
2021-10-09 13:38:43 +0200

    assignment1: add first part of Board implementation

--- assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/Board.java
+++ assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/Board.java
@@ -5,6 +5,16 @@ import static ch.uzh.group8.assignment1.exercise3.BoardCoordinates.*;
 import java.util.*;
 
 public class Board {
+  private final Map<Row, Map<Column, Piece>> boardMatrix;
+
+  public Board() {
+    boardMatrix = new HashMap<>();
+    boardMatrix.put(Row.ROW_1, Map.of(Column.A, new Piece(Player.PLAYER_WHITE, false)));
+    boardMatrix.put(Row.ROW_3, Map.of(Column.A, new Piece(Player.PLAYER_WHITE, false)));
+    boardMatrix.put(Row.ROW_2, Map.of(Column.B, new Piece(Player.PLAYER_WHITE, false)));
+    boardMatrix.put(Row.ROW_8, Map.of(Column.A, new Piece(Player.PLAYER_RED, false)));
+  }
+
   public void executeMove(Move move) {}
 
   public List<Piece> getAllPieces() {
@@ -12,11 +22,15 @@ public class Board {
   }
 
   public Optional<Piece> getPieceAt(BoardCoordinates boardCoordinates) {
-    return Optional.empty();
+    var columnOptionalMap = boardMatrix.get(boardCoordinates.row());
+    if (columnOptionalMap == null) {
+      return Optional.empty();
+    }
+    return Optional.ofNullable(columnOptionalMap.get(boardCoordinates.column()));
   }
 
   /** Important that LinkedHashMap is used: this preserves the order */
-  public Map<Row, Map<Column, Optional<Piece>>> getBoard() {
-    return new LinkedHashMap<>();
+  public Map<Row, Map<Column, Piece>> getBoard() {
+    return boardMatrix;
   }
 }
