8ebfe1461787a77f02e61b67ee0cb5c91a9acc77
BacLuc <lucius.bachmann@gmx.ch>
2021-10-13 22:27:01 +0200

    fix format of TargetFieldEmpty

--- assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/TargetFieldEmpty.java
+++ assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/TargetFieldEmpty.java
@@ -3,14 +3,12 @@ package ch.uzh.group8.assignment1.exercise3.movevalidator;
 import ch.uzh.group8.assignment1.exercise3.Board;
 import ch.uzh.group8.assignment1.exercise3.Move;
 import ch.uzh.group8.assignment1.exercise3.Piece;
-
 import java.util.Optional;
 
 public class TargetFieldEmpty implements MoveValidator {
   @Override
   public boolean validate(Move move, Board board) {
     Optional<Piece> pieceAt = board.getPieceAt(move.start());
-
 
     Piece piece = pieceAt.get();
 
