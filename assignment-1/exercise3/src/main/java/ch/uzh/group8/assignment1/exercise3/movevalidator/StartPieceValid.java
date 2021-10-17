bb6c36d529d118ff6a7d4d7e16e7bf3c1bee1520
islem <mohamedislem.mdimagh@uzh.ch>
2021-10-17 14:13:20 +0200

    Added StartPieceValid implementation

--- assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/StartPieceValid.java
+++ assignment-1/exercise3/src/main/java/ch/uzh/group8/assignment1/exercise3/movevalidator/StartPieceValid.java
@@ -1,11 +1,24 @@
 package ch.uzh.group8.assignment1.exercise3.movevalidator;
 
-import ch.uzh.group8.assignment1.exercise3.Board;
+import ch.uzh.group8.assignment1.exercise3.*;
 import ch.uzh.group8.assignment1.exercise3.Move;
 
 public class StartPieceValid implements MoveValidator {
   @Override
   public boolean validate(Move move, Board board) {
-    return true;
+    var start = move.start();
+
+    var row = BoardCoordinates.Row.values();
+    var col = BoardCoordinates.Column.values();
+
+    var startPiece = board.getPieceAt(new BoardCoordinates(row[start.row().ordinal()], col[start.column().ordinal()]));
+
+    if (startPiece.isEmpty()){
+      return false;
+    }
+
+    return startPiece.get().owner() == move.player();
+
   }
+
 }
