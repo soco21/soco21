df351d1e6a75954850cc384596252fa233077e36
BacLuc <lucius.bachmann@gmx.ch>
2021-10-18 09:23:43 +0200

    MoveLength: add test

---
+++ assignment-1/exercise3/src/test/java/ch/uzh/group8/assignment1/exercise3/movevalidator/MoveLengthTest.java
@@ -0,0 +1,132 @@
+package ch.uzh.group8.assignment1.exercise3.movevalidator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import ch.uzh.group8.assignment1.exercise3.Board;
+import ch.uzh.group8.assignment1.exercise3.BoardCoordinates;
+import ch.uzh.group8.assignment1.exercise3.Move;
+import ch.uzh.group8.assignment1.exercise3.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MoveLengthTest {
+  private MoveLength moveLength;
+  private Board board;
+
+  @BeforeEach
+  public void setup() {
+    moveLength = new MoveLength();
+    board = new Board();
+  }
+
+  @Test
+  public void return_true_if_player_moves_1_row() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_2, BoardCoordinates.Column.A));
+    assertThat(moveLength.validate(move, board), is(true));
+  }
+
+  @Test
+  public void return_true_if_player_moves_2_row() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_3, BoardCoordinates.Column.A));
+    assertThat(moveLength.validate(move, board), is(true));
+  }
+
+  @Test
+  public void return_false_if_player_moves_3_row() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_4, BoardCoordinates.Column.A));
+    assertThat(moveLength.validate(move, board), is(false));
+  }
+
+  @Test
+  public void return_true_if_player_moves_7_row() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_8, BoardCoordinates.Column.A));
+    assertThat(moveLength.validate(move, board), is(false));
+  }
+
+  @Test
+  public void return_true_if_player_moves_1_col() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.B));
+    assertThat(moveLength.validate(move, board), is(true));
+  }
+
+  @Test
+  public void return_true_if_player_moves_2_col() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.C));
+    assertThat(moveLength.validate(move, board), is(true));
+  }
+
+  @Test
+  public void return_false_if_player_moves_3_col() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.D));
+    assertThat(moveLength.validate(move, board), is(false));
+  }
+
+  @Test
+  public void return_true_if_player_moves_7_col() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.H));
+    assertThat(moveLength.validate(move, board), is(false));
+  }
+
+  @Test
+  public void return_true_if_player_moves_1_row_back() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_2, BoardCoordinates.Column.A),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A));
+    assertThat(moveLength.validate(move, board), is(true));
+  }
+
+  @Test
+  public void return_true_if_player_moves_1_col_back() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.B),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.A));
+    assertThat(moveLength.validate(move, board), is(true));
+  }
+
+  @Test
+  public void return_true_if_player_moves_1_col_and_2_rows() {
+    var move =
+        new Move(
+            Player.PLAYER_RED,
+            new BoardCoordinates(BoardCoordinates.Row.ROW_1, BoardCoordinates.Column.B),
+            new BoardCoordinates(BoardCoordinates.Row.ROW_3, BoardCoordinates.Column.A));
+    assertThat(moveLength.validate(move, board), is(true));
+  }
+}
