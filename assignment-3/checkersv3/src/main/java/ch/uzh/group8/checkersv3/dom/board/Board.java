ca39d4fc127daa483ab304ce9a386da42cd2c993
BacLuc <lucius.bachmann@gmx.ch>
2021-11-23 16:36:09 +0100

    checkersv3: implement command pattern on Board

--- assignment-3/checkersv3/src/main/java/ch/uzh/group8/checkersv3/dom/board/Board.java
+++ assignment-3/checkersv3/src/main/java/ch/uzh/group8/checkersv3/dom/board/Board.java
@@ -1,6 +1,7 @@
 package ch.uzh.group8.checkersv3.dom.board;
 
 import ch.uzh.group8.checkersv3.dom.*;
+import ch.uzh.group8.checkersv3.util.Tuple;
 import java.util.*;
 
 public class Board {
@@ -8,27 +9,8 @@ public class Board {
   private final Store store = new Store();
 
   public void executeMove(Move move) {
-    if (move.jumpGambleResult() == JumpGambleResult.LOST) {
-      store.removePiece(move.start());
-      boardObservers.forEach(boardObserver -> boardObserver.boardChanged(this));
-      return;
-    }
-    Piece piece = store.getPieceAt(move.start()).orElseThrow();
-    store.removePiece(move.start());
-    store.addPiece(move.end(), piece);
-
-    Optional<BoardCoordinates> coordinatesBetween = move.getCoordinatesBetween();
-    coordinatesBetween.ifPresent(store::removePiece);
-
-    if (move.player() == Player.PLAYER_WHITE) {
-      if (move.end().row().isLastRow()) {
-        store.addPiece(move.end(), new Piece(Player.PLAYER_WHITE, true));
-      }
-    } else {
-      if (move.end().row().isFirstRow()) {
-        store.addPiece(move.end(), new Piece(Player.PLAYER_RED, true));
-      }
-    }
+    Command command = createCommand(move);
+    command.execute();
     boardObservers.forEach(boardObserver -> boardObserver.boardChanged(this));
   }
 
@@ -38,5 +20,31 @@ public class Board {
 
   public void registerObserver(BoardObserver boardObserver) {
     boardObservers.add(boardObserver);
+  }
+
+  private Command createCommand(Move move) {
+    Piece pieceAtStart = store.getPieceAt(move.start()).orElseThrow();
+    Tuple<BoardCoordinates, Piece> start = Tuple.of(move.start(), pieceAtStart);
+    if (move.jumpGambleResult() == JumpGambleResult.LOST) {
+      return new JumpGambleLostMove(store, start);
+    }
+
+    boolean convertToKing = isConvertToKing(move);
+    Piece pieceAtEnd = new Piece(move.player(), pieceAtStart.isKing() || convertToKing);
+    Tuple<BoardCoordinates, Piece> end = Tuple.of(move.end(), pieceAtEnd);
+    if (move.isJumpMove()) {
+      Piece pieceBetween = move.getCoordinatesBetween().flatMap(store::getPieceAt).orElseThrow();
+      return new JumpMove(
+          store, start, Tuple.of(move.getCoordinatesBetween().orElseThrow(), pieceBetween), end);
+    } else {
+      return new SimpleMove(store, start, end);
+    }
+  }
+
+  private boolean isConvertToKing(Move move) {
+    if (move.player() == Player.PLAYER_WHITE) {
+      return move.end().row().isLastRow();
+    }
+    return move.end().row().isFirstRow();
   }
 }
---
+++ assignment-3/checkersv3/src/main/java/ch/uzh/group8/checkersv3/dom/board/Command.java
@@ -0,0 +1,5 @@
+package ch.uzh.group8.checkersv3.dom.board;
+
+interface Command {
+  void execute();
+}
