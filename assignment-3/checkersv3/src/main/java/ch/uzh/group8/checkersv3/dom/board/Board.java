043ca5105643cd8a8a4ba5297a84e141aa34bfaa
BacLuc <lucius.bachmann@gmx.ch>
2021-11-23 15:26:03 +0100

    checkersv3: extract delegate Store from Board

--- assignment-3/checkersv3/src/main/java/ch/uzh/group8/checkersv3/dom/board/Board.java
+++ assignment-3/checkersv3/src/main/java/ch/uzh/group8/checkersv3/dom/board/Board.java
@@ -1,148 +1,42 @@
 package ch.uzh.group8.checkersv3.dom.board;
 
-import static ch.uzh.group8.checkersv3.dom.BoardCoordinates.Column;
-import static ch.uzh.group8.checkersv3.dom.BoardCoordinates.Row;
-
 import ch.uzh.group8.checkersv3.dom.*;
 import java.util.*;
 
 public class Board {
-  private final Map<Row, Map<Column, Piece>> boardMatrix;
   private final List<BoardObserver> boardObservers = new ArrayList<>();
-
-  public Board() {
-    boardMatrix = new HashMap<>();
-    boardMatrix.put(
-        Row.ROW_1,
-        Map.of(
-            Column.A,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.C,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.E,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.G,
-            new Piece(Player.PLAYER_WHITE, false)));
-    boardMatrix.put(
-        Row.ROW_3,
-        Map.of(
-            Column.A,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.C,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.E,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.G,
-            new Piece(Player.PLAYER_WHITE, false)));
-    boardMatrix.put(
-        Row.ROW_2,
-        Map.of(
-            Column.B,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.D,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.F,
-            new Piece(Player.PLAYER_WHITE, false),
-            Column.H,
-            new Piece(Player.PLAYER_WHITE, false)));
-
-    boardMatrix.put(
-        Row.ROW_8,
-        Map.of(
-            Column.B,
-            new Piece(Player.PLAYER_RED, false),
-            Column.D,
-            new Piece(Player.PLAYER_RED, false),
-            Column.F,
-            new Piece(Player.PLAYER_RED, false),
-            Column.H,
-            new Piece(Player.PLAYER_RED, false)));
-    boardMatrix.put(
-        Row.ROW_6,
-        Map.of(
-            Column.B,
-            new Piece(Player.PLAYER_RED, false),
-            Column.D,
-            new Piece(Player.PLAYER_RED, false),
-            Column.F,
-            new Piece(Player.PLAYER_RED, false),
-            Column.H,
-            new Piece(Player.PLAYER_RED, false)));
-    boardMatrix.put(
-        Row.ROW_7,
-        Map.of(
-            Column.A,
-            new Piece(Player.PLAYER_RED, false),
-            Column.C,
-            new Piece(Player.PLAYER_RED, false),
-            Column.E,
-            new Piece(Player.PLAYER_RED, false),
-            Column.G,
-            new Piece(Player.PLAYER_RED, false)));
-  }
+  private final Store store = new Store();
 
   public void executeMove(Move move) {
     if (move.jumpGambleResult() == JumpGambleResult.LOST) {
-      removePiece(move.start());
+      store.removePiece(move.start());
       boardObservers.forEach(boardObserver -> boardObserver.boardChanged(this));
       return;
     }
-    Piece piece = getPieceAt(move.start()).orElseThrow();
-    removePiece(move.start());
-    addPiece(move.end(), piece);
+    Piece piece = store.getPieceAt(move.start()).orElseThrow();
+    store.removePiece(move.start());
+    store.addPiece(move.end(), piece);
 
     Optional<BoardCoordinates> coordinatesBetween = move.getCoordinatesBetween();
-    coordinatesBetween.ifPresent(this::removePiece);
+    coordinatesBetween.ifPresent(store::removePiece);
 
     if (move.player() == Player.PLAYER_WHITE) {
       if (move.end().row().isLastRow()) {
-        addPiece(move.end(), new Piece(Player.PLAYER_WHITE, true));
+        store.addPiece(move.end(), new Piece(Player.PLAYER_WHITE, true));
       }
     } else {
       if (move.end().row().isFirstRow()) {
-        addPiece(move.end(), new Piece(Player.PLAYER_RED, true));
+        store.addPiece(move.end(), new Piece(Player.PLAYER_RED, true));
       }
     }
     boardObservers.forEach(boardObserver -> boardObserver.boardChanged(this));
   }
 
   public Optional<Piece> getPieceAt(BoardCoordinates boardCoordinates) {
-    Map<Column, Piece> columnOptionalMap = boardMatrix.get(boardCoordinates.row());
-    if (columnOptionalMap == null) {
-      return Optional.empty();
-    }
-    return Optional.ofNullable(columnOptionalMap.get(boardCoordinates.column()));
+    return store.getPieceAt(boardCoordinates);
   }
 
   public void registerObserver(BoardObserver boardObserver) {
     boardObservers.add(boardObserver);
-  }
-
-  private void removePiece(BoardCoordinates start) {
-    boardMatrix.compute(
-        start.row(),
-        (row, columnPieceMap) -> {
-          if (columnPieceMap == null) {
-            return new HashMap<>();
-          } else {
-            HashMap<Column, Piece> columnPieceHashMap = new HashMap<>(columnPieceMap);
-            columnPieceHashMap.remove(start.column());
-            return columnPieceHashMap;
-          }
-        });
-  }
-
-  private void addPiece(BoardCoordinates boardCoordinates, Piece piece) {
-    boardMatrix.compute(
-        boardCoordinates.row(),
-        (row, columnPieceMap) -> {
-          if (columnPieceMap == null) {
-            return Map.of(boardCoordinates.column(), piece);
-          } else {
-            HashMap<Column, Piece> columnPieceHashMap = new HashMap<>(columnPieceMap);
-            columnPieceHashMap.put(boardCoordinates.column(), piece);
-            return columnPieceHashMap;
-          }
-        });
   }
 }
