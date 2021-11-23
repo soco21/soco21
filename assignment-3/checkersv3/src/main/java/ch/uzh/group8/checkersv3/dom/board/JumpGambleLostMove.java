d10cd4167ce0ca7b8cdd096a4d97aa2ebc765d06
BacLuc <lucius.bachmann@gmx.ch>
2021-11-23 16:36:09 +0100

    checkersv3: implement command pattern on Board

---
+++ assignment-3/checkersv3/src/main/java/ch/uzh/group8/checkersv3/dom/board/JumpGambleLostMove.java
@@ -0,0 +1,21 @@
+package ch.uzh.group8.checkersv3.dom.board;
+
+import ch.uzh.group8.checkersv3.dom.BoardCoordinates;
+import ch.uzh.group8.checkersv3.dom.Piece;
+import ch.uzh.group8.checkersv3.util.Tuple;
+
+@SuppressWarnings("ClassCanBeRecord")
+class JumpGambleLostMove implements Command {
+  private final Store store;
+  private final Tuple<BoardCoordinates, Piece> start;
+
+  JumpGambleLostMove(Store store, Tuple<BoardCoordinates, Piece> start) {
+    this.store = store;
+    this.start = start;
+  }
+
+  @Override
+  public void execute() {
+    store.removePiece(start.getKey());
+  }
+}
